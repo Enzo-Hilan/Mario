07df9163560cfa046556f9e18f1c29ab61411253
Enzo Hilan Santos de Lima <enzohilan171006@gmail.com>
2022-06-27 22:03:50 -0300

    Programando o contador de pontuação

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-const player = document.querySelector('.player');
-const pipe = document.querySelector('.pipe');
-const menu = document.querySelector('.bg');
+const player = document.querySelector('.player'); //Player
+const pipe = document.querySelector('.pipe'); //Zombie
+const menu = document.querySelector('.bg'); //BackGround
+const contador = document.getElementById("contagem") //Contador
 const contagem = 0;
 
 
+//Configurando pulo
 const jump = () => {
     player.classList.add('jump');
 
@@ -12,10 +14,22 @@ const jump = () => {
     }, 500);
 }
 
+// Cria a função para contar os segundos
+var segundos = 0;
+function ContarSegundos(){
+    segundos = segundos + 1;
+    contador.innerHTML = segundos;
+    return segundos
+}
+
+//Cria um loop que irá rodar durante todo o jogo
 function loop() {
     const loop = setInterval(() => {
         const pipePosition = pipe.offsetLeft;
         const playerPosition = +window.getComputedStyle(player).bottom.replace('px', '');
+
+        //Chama a função 'ContarSegundos'
+        const repeticao = setTimeout(ContarSegundos(), 10)
 
         if (pipePosition <= 120 && pipePosition > 0 && playerPosition < 60) {
 
@@ -28,10 +42,12 @@ function loop() {
             player.src = './images/game_over.png';
             player.style.width = '65px'
             player.style.marginLeft = '50px'
+            player.style.bottom = '0'
 
             menu.style.display = 'flex'
 
             clearInterval(loop);
+            segundos = 0;
 
         } 
 
